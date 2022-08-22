5c06249fb4b4c77ec4d5828ec32846d897fb5f42
h16ki <h16kidev@gmail.com>
2022-08-22 02:04:49 +0000

    A code and fig of pi improved

--- src/pi.py
+++ src/pi.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -8,18 +9,23 @@ fig = plt.figure()
 
 # constant
 N: int = 5_000
+accepted: list[list[float]] = [[], []]
+rejected: list[list[float]] = [[], []]
 
 def calc_pi():
-    accepted: int = 0
+
     for i in tqdm(range(N)):
         x, y = np.random.rand(2)
         if x ** 2 + y ** 2 <= 1.0:
-            accepted += 1
-            plt.plot(x, y, marker="o", c="g", alpha=0.3, mec="g")
+            accepted[0].append(x)
+            accepted[1].append(y)
+            # plt.plot(x, y, marker="o", c="g", alpha=0.3, mec="g")
         else:
-            plt.plot(x, y, marker="o", c="r", alpha=0.3, mec="r")
+            rejected[0].append(x)
+            rejected[1].append(y)
+            # plt.plot(x, y, marker="o", c="r", alpha=0.3, mec="r")
 
-    pi = 4.0 * accepted / N
+    pi = 4.0 * len(accepted[0]) / N
     return pi
 
 if __name__ == "__main__":
@@ -35,6 +41,13 @@ if __name__ == "__main__":
     print(result)
 
     print("Save fig.")
+    plt.scatter(accepted[0], accepted[1], c="g", alpha=0.3, edgecolors="g")
+    plt.scatter(rejected[0], rejected[1], c="r", alpha=0.3, edgecolors="r")
+
+    x = np.linspace(0, 1, 300)
+    y = np.sqrt(1 - x ** 2)
+    plt.plot(x, y, c="k", label="Boundary")
     plt.title(f"Trials: {N}, " + result)
+    plt.legend()
     plt.grid()
     plt.savefig("../fig/monte_carlo_pi.png")
